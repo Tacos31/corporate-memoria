abb1ca5f50a056bff7301699a6d67aca8468c454
Tacos31 <sabathieralexandre@yahoo.fr>
2018-07-09 13:32:44 +0200

    oai

--- routes/index.js
+++ routes/index.js
@@ -44,31 +44,37 @@ router.get('/contact', function(req, res, next) {
 });
 
 router.post('/contact', function(req, res, next) {
-  const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-          user: `corporate.memoria@gmail.com`,
-          pass: `Corporate@memoria`
-    }
-  })
-  const mailOptions = {
-      from: `${req.body.email}`,
-      to: 'corporate.memoria@outlook.com',
-      subject: `Message de ${req.body.name} depuis Corporate Memoria`,
-      text: `Message :\n\n${req.body.message}\n\nContactez moi :\n\nTelephone : ${req.body.phone} \nEmail : ${req.body.email}`,
-      replyTo: `${req.body.email}`
-    }
-  //console.log(mailOptions);
-  transporter.sendMail(mailOptions, function(err, res) {
-    if (err) {
-      console.error('there was an error: ', err);
-    } else {
-      //console.log('here is the res: ', res)
-    }
-  })
-  res.render('contact', {
-    successMessage: "email envoyé",
-  });
+  if (req.body.email=="" || req.body.name == "" || req.body.message == "") {
+    res.render('contact', {
+      errorMessage: "Veuillez remplir les champs obligatoires"
+    });
+  } else {
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+            user: `corporate.memoria@gmail.com`,
+            pass: `Corporate@memoria`
+      }
+    })
+    const mailOptions = {
+        from: `${req.body.email}`,
+        to: 'corporate.memoria@outlook.com',
+        subject: `Message de ${req.body.name} depuis Corporate Memoria`,
+        text: `Message :\n\n${req.body.message}\n\nContactez moi :\n\nTelephone : ${req.body.phone} \nEmail : ${req.body.email}`,
+        replyTo: `${req.body.email}`
+      }
+    //console.log(mailOptions);
+    transporter.sendMail(mailOptions, function(err, res) {
+      if (err) {
+        console.error('there was an error: ', err);
+      } else {
+        //console.log('here is the res: ', res)
+      }
+    })
+    res.render('contact', {
+      successMessage: "Email envoyé"
+    });
+  }
 });
 
 module.exports = router;
